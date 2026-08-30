3b83fa7fdebb6d7c2652373359ff66f940af7750
dphack1987 <proyectoreact894@gmail.com>
2026-08-30 14:07:32 -0500

    Configurar rutas relativas para compatibilidad con apertura directa de archivos

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: './', // Rutas relativas para abrir archivo directamente
   build: {
     outDir: 'dist'
   }
